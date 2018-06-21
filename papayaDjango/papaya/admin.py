9feca9d171ec5c9bf9c13cccaa9cb3dc01d19c82
Shem <carl.ipanag@gmail.com>
2018-06-21 09:37:39 +0800

    add projects

--- papayaDjango/papaya/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Category, Blog, User
-
-
-admin.site.register(Category)
-admin.site.register(Blog)
-admin.site.register(User)
--- papayaDjango/papayaDjango/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import include, path
-from django.contrib import admin
-
-
-urlpatterns = [
-    path('', include('papaya.urls')),
-    path('admin/', admin.site.urls),
-]
